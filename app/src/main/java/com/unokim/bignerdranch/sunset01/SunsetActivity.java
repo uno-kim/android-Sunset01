39748496aaac761ae6228a18b20ce1dd5fa9d37b
Min Sung Kim <ms.uno.kim@gmail.com>
2018-01-26 15:45:47 +0900

    initial commit

---
+++ app/src/main/java/com/unokim/bignerdranch/sunset01/SunsetActivity.java
@@ -0,0 +1,13 @@
+package com.unokim.bignerdranch.sunset01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunsetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sunset);
+    }
+}
